7341dce9a974d0d94e38e9e22e8fe0918a502114
96isasva <mustigmat@gmail.com>
2016-02-17 15:57:29 +0100

    lektion 17/2
    adding the base code for the game

---
+++ player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent (typeof (Controller2D))]
+public class player : MonoBehaviour {
+
+	Controller2D controller;
+
+	void Start () {
+		controller = GetComponent<Controller2D> ();
+	}
+
+	
+	}
+}
